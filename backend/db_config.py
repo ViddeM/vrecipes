a899fb4263f798d5216653669e26d8c0cab8112d
Vidar Magnusson <git@vidarmagnusson.com>
2020-07-19 17:18:29 +0200

    CHanged from vrecept => vrecipes

--- backend/db_config.py
+++ backend/db_config.py
@@ -1,7 +1,7 @@
 import os
 
-POSTGRES_USER = os.environ.get("VRECEPT_POSTGRES_USER", "vrecept")
+POSTGRES_USER = os.environ.get("VRECEPT_POSTGRES_USER", "vrecipes")
 POSTGRES_PASSWORD = os.environ.get("VRECEPT_POSTGRES_PASSWORD", "password")
 POSTGRES_HOST = os.environ.get("VRECEPT_POSTGRES_HOST", "localhost")
 POSTGRES_PORT = os.environ.get("VRECEPT_POSTGRES_PORT", "5432")
-POSTGRES_DB = os.environ.get("VRECEPT_POSTGRES_DB", "vrecept")
+POSTGRES_DB = os.environ.get("VRECEPT_POSTGRES_DB", "vrecipes")
