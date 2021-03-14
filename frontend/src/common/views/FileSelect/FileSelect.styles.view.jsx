d5996f44fe185844f915542a0d07afa625d2df5c
Vidar Magnusson <git@vidarmagnusson.com>
2021-03-14 16:37:23 +0100

    Update...

--- frontend/src/common/views/FileSelect/FileSelect.styles.view.jsx
+++ frontend/src/common/views/FileSelect/FileSelect.styles.view.jsx
@@ -2,7 +2,9 @@ import styled from "styled-components"
 import {DigitButton} from "@cthit/react-digit-components";
 
 export const FileSelectContainer = styled.div`
-  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const FileSelectInput = styled.input`
@@ -10,5 +12,6 @@ export const FileSelectInput = styled.input`
 `;
 
 export const FileSelectButton = styled(DigitButton)`
-
+  margin-right: auto;
+  margin-left: auto;
 `;
