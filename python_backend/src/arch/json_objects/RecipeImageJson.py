22c30cb050455b9c977397b3dd1767f13c71b149
Vidar Magnusson <git@vidarmagnusson.com>
2021-03-23 17:15:02 +0100

    Remove old backend

--- python_backend/src/arch/json_objects/RecipeImageJson.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from uuid import UUID
-
-
-@dataclass
-class RecipeImageJson:
-    id: UUID
-
--- python_backend/src/arch/query/ConfigQueries.py
+++
@@ -1,9 +0,0 @@
-from pony.orm import db_session
-
-from db import Config
-
-
-@db_session
-def get_config(key):
-    return Config.get(key=key).value
-
