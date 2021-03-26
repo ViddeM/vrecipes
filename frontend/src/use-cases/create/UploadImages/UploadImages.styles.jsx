8f612402304be24137f233007f4f3906b4d1bb19
Vidar Magnusson <git@vidarmagnusson.com>
2021-03-26 18:43:14 +0100

    Tweaked margin aroud images on create screen

--- frontend/src/use-cases/create/UploadImages/UploadImages.styles.jsx
+++ frontend/src/use-cases/create/UploadImages/UploadImages.styles.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 export const StyledImage = styled.img`
-  margin: 10px;
+  margin: 10px 5px;
   border-radius: 5px;
   border: 1px solid black;
   text-align: center;
