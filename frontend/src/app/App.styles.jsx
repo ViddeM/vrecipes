b8108bb6443ba4bc98adc56892985eb29c7633f3
Vidar Magnusson <git@vidarmagnusson.com>
2021-03-31 18:48:00 +0200

    Updated logo

--- frontend/src/app/App.styles.jsx
+++ frontend/src/app/App.styles.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
-import {AppBar} from "@material-ui/core";
+import {AppBar, IconButton} from "@material-ui/core";
+import {ReactComponent as VRecipesLogo} from "../resources/images/group_1_transparent.svg"
+import {NavLink} from "react-router-dom";
 
 export const MainContainer = styled.div`
   display: flex;
@@ -26,3 +28,34 @@ export const HeaderContainer = styled.div`
   padding-left: 10px;
   padding-right: 10px;
 `;
+
+export const LogoTitleContainer = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Logo = styled(VRecipesLogo)`
+  height: 100%;
+  width: auto;
+`
+
+export const LogoButton = styled(IconButton)`
+  height: 100%;
+  
+  &.MuiIconButton-sizeSmall .MuiIconButton-label {
+    height: 100%;
+  }
+`;
+
+export const EscapeHatch = styled(NavLink)`
+  height: calc(100% - 10px);
+  margin-right: 10px;
+     border-radius: 100%;
+  
+  &:active {
+     background-color: rgba(0,0,0,0.2);
+  }
+`
