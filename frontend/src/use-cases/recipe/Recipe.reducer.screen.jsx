9c8b8806315fbd13f6e85b83db3643ec4a436364
Vidar Magnusson <git@vidarmagnusson.com>
2021-04-04 01:44:20 +0200

    Removed mock recipe from code

--- frontend/src/use-cases/recipe/Recipe.reducer.screen.jsx
+++ frontend/src/use-cases/recipe/Recipe.reducer.screen.jsx
@@ -5,67 +5,6 @@ import {
     EDIT_RECIPE
 } from "./screens/RecipeCard/views/recipe-footer/RecipeFooter.actions.view";
 
-const mockRecipe = {
-    id: "asd123",
-    name: "Chokladbollar",
-    imageSrc: "chokladbollar.jpg",
-    description: " Dessa chokladbollar får en liten karamellsmak av att smöret först får puttra. Rulla chokladbollarna i kokos eller pärlsocker, det som du själv föredrar. ",
-    steps: [
-        {
-            number: 1,
-            description: "Smält smöret"
-        },
-        {
-            number: 2,
-            description: "Rör ihop socker, vaniljsocker, kakao, havregryn, kaffe och smöret. Ställ in i kylskåp så att smeten stelnar lite, ca 1 timme."
-        }, {
-            number: 3,
-            description: "Forma bollar. Rulla i pärlsocker (gärna mångfärgat), kokos eller annan garnering. "
-        }
-    ],
-    ingredients: [
-        {
-            name: "smör",
-            amount: "100",
-            unit: "g"
-        },
-        {
-            name: "socker",
-            amount: "1",
-            unit: "dl"
-        },
-        {
-            name: "vaniljsocker",
-            amount: "1",
-            unit: "msk"
-        },
-        {
-            name: "havregryn",
-            amount: "3",
-            unit: "dl"
-        },
-        {
-            name: "kallt starkt kaffe",
-            amount: "3",
-            unit: "msk"
-        },
-        {
-            name: "pärlsocker eller annan garnering",
-            amount: "",
-            unit: ""
-        }
-    ],
-    estimatedTime: 90,
-    ovenTemperature: 200,
-    images: [
-        {
-            id: "asd123",
-            url: "asd.com"
-        }
-    ],
-    author: {}
-}
-
 const initialState = {
     recipe: null,
     error: null,
