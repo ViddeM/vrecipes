6ab939ae1814181a58111082c306ecb72d77e88f
Vidar Magnusson <git@vidarmagnusson.com>
2021-05-30 16:35:24 +0200

    Finished getRecipeBooks endpoint

--- backend/internal/process/getRecipeBooksProcess.go
+++ backend/internal/process/getRecipeBooksProcess.go
@@ -1,6 +1,7 @@
 package process
 
 import (
+	"github.com/georgysavva/scany/pgxscan"
 	"github.com/viddem/vrecipes/backend/internal/db/queries"
 	"github.com/viddem/vrecipes/backend/internal/db/tables"
 )
@@ -42,5 +43,25 @@ func GetRecipeBooks() (*RecipeBooksJson, error) {
 	shortRecipeBooks := make([]ShortRecipeBookJson, 0)
 	for _, book := range recipeBooks {
 		image, err := queries.GetImageForRecipeBook(book.ID)
+
+		imageUrl := ""
+		if err != nil {
+			if pgxscan.NotFound(err) == false {
+				return nil, err
+			}
+		} else {
+			imageUrl = imageNameToPath(image.ID, image.Name)
+		}
+		
+		user, err := queries.GetUser(book.CreatedBy)
+		if err != nil {
+			return nil, err
+		}
+		
+		shortRecipeBooks = append(shortRecipeBooks, toShortRecipeBookJson(book, user, imageUrl))
 	}
+
+	return &RecipeBooksJson{
+		RecipeBooks: shortRecipeBooks,
+	}, nil
 }
