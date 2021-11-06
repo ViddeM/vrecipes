2a41055761d6fe163e5e801396f5f7151162cddf
Vidar Magnusson <git@vidarmagnusson.com>
2021-11-06 02:02:46 +0100

    Now uses real data on tags list page

--- frontend/src/use-cases/search/RecipeTags/RecipeTags.actions.jsx
+++ frontend/src/use-cases/search/RecipeTags/RecipeTags.actions.jsx
@@ -1,2 +1,4 @@
 export const ON_RECIPE_TAGS_SEARCH_FIELD_CHANGE = "on_recipe_tags_search_field_change";
 export const ON_SET_CREATING_TAG = "on_set_creating_tag";
+export const LOAD_TAGS_SUCCESSFUL = "load_tags_successful";
+export const LOAD_TAGS_FAILED = "load_tags_failed";
--- frontend/src/use-cases/search/RecipeTags/RecipeTags.container.jsx
+++ frontend/src/use-cases/search/RecipeTags/RecipeTags.container.jsx
@@ -1,6 +1,7 @@
 import {RecipeTags} from "./RecipeTags";
 import {connect} from "react-redux";
 import {
+    loadTags,
     recipeTagsSearchChanged,
     setCreatingTag
 } from "./RecipeTags.action-creators";
@@ -8,12 +9,14 @@ import {
 const mapStateToProps = state => ({
     tags: state.root.recipeTags.filteredTags,
     searchText: state.root.recipeTags.searchText,
-    creatingTag: state.root.recipeTags.creatingTag
+    creatingTag: state.root.recipeTags.creatingTag,
+    update: state.root.recipeTags.update
 })
 
 const mapDispatchToProps = dispatch => ({
     onSearch: text => dispatch(recipeTagsSearchChanged(text)),
-    setCreatingTag: creatingTag => dispatch(setCreatingTag(creatingTag))
+    setCreatingTag: creatingTag => dispatch(setCreatingTag(creatingTag)),
+    loadTags: () => dispatch(loadTags())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(RecipeTags);
