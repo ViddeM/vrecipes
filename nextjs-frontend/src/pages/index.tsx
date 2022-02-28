9af2794545b2806c86bd4e18b13bb5b00115a182
Vidar Magnusson <git@vidarmagnusson.com>
2022-02-28 23:36:55 +0100

    Restructure nextjs project

---
+++ nextjs-frontend/src/pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../../styles/Home.module.scss'
+import Header from "../components/Header";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Vrecipes</title>
+        <link rel="icon" href="/vrecipes_logo.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <Header />
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
