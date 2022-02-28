c16cb4fdada91c25d1f44da10e28494cf71bd34a
Vidar Magnusson <git@vidarmagnusson.com>
2022-03-01 00:40:18 +0100

    More nextjs setup

--- nextjs-frontend/src/components/Header.tsx
+++ nextjs-frontend/src/components/Header.tsx
@@ -1,11 +1,15 @@
 import styles from "./Header.module.scss";
 
 const Header = () => {
-  return (
-    <div className={styles.header}>
-      HEALLO
-    </div>
-  )
+    return (
+        <div className={styles.headerContainer}>
+            <div className={styles.header}>
+                <h1>
+                    VRecipes
+                </h1>
+            </div>
+        </div>
+    )
 }
 
 export default Header;
