c79ae7701041b9ffa90ec86fbe943e292847b79c
Vidar Magnusson <git@vidarmagnusson.com>
2022-03-06 16:30:19 +0100

    Add temperatures/times for select mock recipes

--- mock_data/mockdata.sql
+++ mock_data/mockdata.sql
@@ -8,12 +8,12 @@ VALUES ('test@test.com', 'facebook', '7cc2fc04-7379-4361-9f4e-079054933d2f');
 
 -- Insert recipes
 INSERT INTO recipe (id, name, unique_name, description, oven_temp, estimated_time, deleted, created_by)
-VALUES  ('133cdcc3-5b5b-440e-95f5-0d5dd4f8f78b', 'A recipe', 'a_recipe', 'Desc', 0, 0, false, '7cc2fc04-7379-4361-9f4e-079054933d2f'), 
-        ('1e538581-9e9a-4da7-80f6-3127f246c17f', 'A recipe 2', 'a_recipe_2', 'Desc', 0, 0, false, '7cc2fc04-7379-4361-9f4e-079054933d2f'),
+VALUES  ('133cdcc3-5b5b-440e-95f5-0d5dd4f8f78b', 'A recipe', 'a_recipe', 'Desc', 200, 35, false, '7cc2fc04-7379-4361-9f4e-079054933d2f'), 
+        ('1e538581-9e9a-4da7-80f6-3127f246c17f', 'A recipe 2', 'a_recipe_2', 'Desc', 190, 20, false, '7cc2fc04-7379-4361-9f4e-079054933d2f'),
         ('bddb104d-a28e-4bec-97bf-14ad93fc4b20', 'A recipe 3', 'a_recipe_3', 'Desc', 0, 0, false, '7cc2fc04-7379-4361-9f4e-079054933d2f'),
-        ('4213b5e5-0c63-4d31-ba96-f54a574bb040', 'A recipe 4', 'a_recipe_4', 'Desc', 0, 0, false, '7cc2fc04-7379-4361-9f4e-079054933d2f'),
-        ('dca1bc12-0cf0-49e9-b0f9-714f9e0c2975', 'A recipe 5', 'a_recipe_5', 'Desc', 0, 0, false, '7cc2fc04-7379-4361-9f4e-079054933d2f'),
-        ('1281fa39-3209-4cbf-b359-7554e232f323', 'A recipe 6', 'a_recipe_6', 'Desc', 0, 0, false, '7cc2fc04-7379-4361-9f4e-079054933d2f'),
+        ('4213b5e5-0c63-4d31-ba96-f54a574bb040', 'A recipe 4', 'a_recipe_4', 'Desc', 0, 30, false, '7cc2fc04-7379-4361-9f4e-079054933d2f'),
+        ('dca1bc12-0cf0-49e9-b0f9-714f9e0c2975', 'A recipe 5', 'a_recipe_5', 'Desc', 100, 45, false, '7cc2fc04-7379-4361-9f4e-079054933d2f'),
+        ('1281fa39-3209-4cbf-b359-7554e232f323', 'A recipe 6', 'a_recipe_6', 'Desc', 150, 0, false, '7cc2fc04-7379-4361-9f4e-079054933d2f'),
         ('ff9373ea-c5d1-4d05-a299-e0856833131d', 'A recipe 7', 'a_recipe_7', 'Desc', 0, 0, false, '7cc2fc04-7379-4361-9f4e-079054933d2f'),
         ('dfed936b-8e2a-458c-a2d7-1c13c14079d6', 'A recipe with a very long god damned name', 'a_recipe_with_a_very_long_god_damned_name', 'Desc', 0, 0, false, '7cc2fc04-7379-4361-9f4e-079054933d2f');
 
