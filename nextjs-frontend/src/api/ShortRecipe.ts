b0f2e96d3ee4f5c2d8c537fad0a88f2401126989
Vidar Magnusson <git@vidarmagnusson.com>
2022-03-08 22:21:09 +0100

    Formatting & small fixes

--- nextjs-frontend/src/api/ShortRecipe.ts
+++ nextjs-frontend/src/api/ShortRecipe.ts
@@ -1,11 +1,11 @@
-import {Author} from "./Author";
-import {Tag} from "./Tag";
+import { Author } from "./Author";
+import { Tag } from "./Tag";
 
 export interface ShortRecipe {
-    id: string,
-    name: string,
-    uniqueName: string,
-    imageLink: string,
-    author: Author,
-    tags: Tag[],
+  id: string;
+  name: string;
+  uniqueName: string;
+  imageLink: string;
+  author: Author;
+  tags: Tag[];
 }
