3058c32656a539b2e6d03e2f40f82a821f67e4ac
Vidar Magnusson <git@vidarmagnusson.com>
2022-03-09 23:28:27 +0100

    Error handling

--- nextjs-frontend/src/pages/login.tsx
+++ nextjs-frontend/src/pages/login.tsx
@@ -5,21 +5,33 @@ import { useTranslations } from "../hooks/useTranslations";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { Api } from "../api/Api";
+import { useState } from "react";
 
 const Login = () => {
-  let { t } = useTranslations();
+  let { t, translate } = useTranslations();
+  let [error, setError] = useState<string | undefined>(undefined);
 
   return (
     <CardLayout>
       <div className={`card ${styles.loginContainer}`}>
         <h1>{t.login.loginTitle}</h1>
 
+        {error && (
+          <div className="errorText marginTop marginBottom">{error}</div>
+        )}
+
         <Button
           variant="primary"
           size="large"
           className={`${styles.signInButton} ${styles.githubButton}`}
           onClick={() => {
-            Api.user.githubLogin();
+            Api.user.githubLogin().then((val) => {
+              if (val.errorTranslationString) {
+                setError(translate(val.errorTranslationString));
+              } else {
+                setError(t.errors.default);
+              }
+            });
           }}
         >
           <FontAwesomeIcon
