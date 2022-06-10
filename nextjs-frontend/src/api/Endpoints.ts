bcc4ab4bf544b08352eef62a7290be62808f81db
Eric Carlsson <cirecirecire3@gmail.com>
2022-06-10 16:52:58 +0200

    Tag page handles errors correctly

--- nextjs-frontend/src/api/Endpoints.ts
+++ nextjs-frontend/src/api/Endpoints.ts
@@ -7,3 +7,4 @@ export const CREATE_RECIPE_ENDPOINT = "/recipes/create";
 export const EDIT_RECIPE_BASE_ENDPOINT = "/recipes/edit";
 export const LOGIN_ENDPOINT = "/login";
 export const ROOT_ENDPOINT = "/";
+export const TAGS_BASE_ENDPOINT = "/tags";
