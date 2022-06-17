fe6373813ee7a292c186511282387cd5b0380373
Vidar Magnusson <git@vidarmagnusson.com>
2022-06-17 18:46:31 +0200

    Add state management to checkbox

--- nextjs-frontend/src/components/Checkbox.tsx
+++ nextjs-frontend/src/components/Checkbox.tsx
@@ -1,7 +1,23 @@
 import styles from "./Checkbox.module.scss";
+import { InputHTMLAttributes } from "react";
 
-const Checkbox = () => {
-  return <input type="checkbox" className={styles.checkbox} />;
+export type CheckboxProps = InputHTMLAttributes<HTMLInputElement> & {
+  checked: boolean;
+  setChecked: (val: boolean) => void;
+};
+
+const Checkbox = ({ checked, setChecked, ...props }: CheckboxProps) => {
+  return (
+    <input
+      type="checkbox"
+      className={styles.checkbox}
+      checked={checked}
+      onChange={(e) => {
+        setChecked(e.target.checked);
+      }}
+      {...props}
+    />
+  );
 };
 
 export default Checkbox;
