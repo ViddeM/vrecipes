6fc001ac222c2ff8dd38b8ca813da7ccfac46e8d
Vidar Magnusson <git@vidarmagnusson.com>
2022-06-17 16:55:25 +0200

    Add variant/size props for dropdown

--- nextjs-frontend/src/components/Dropdown.tsx
+++ nextjs-frontend/src/components/Dropdown.tsx
@@ -6,23 +6,38 @@ export type SelectOption = {
   value: string;
 };
 
-export type SelectProps = InputHTMLAttributes<HTMLSelectElement> & {
-  options: SelectOption[];
-  onUpdate: (value: string) => void;
-  defaultValue?: string;
+export type DropdownVariant = {
+  variant: "opaque" | "outlined";
 };
+
+export type DropdownSize = {
+  size: "normal" | "auto";
+};
+
+export type SelectProps = InputHTMLAttributes<HTMLSelectElement> &
+  DropdownVariant &
+  DropdownSize & {
+    options: SelectOption[];
+    onUpdate: (value: string) => void;
+    defaultValue?: string;
+  };
 
 const Dropdown: FC<SelectProps> = ({
   options,
   onUpdate,
   defaultValue,
+  size,
+  variant,
   ...props
 }) => {
+  let variantStyle = styles[`dropdown-variant-${variant}`];
+  let sizeStyle = styles[`dropdown-size-${size}`];
+
   return (
     <select
       {...props}
       onChange={(e) => onUpdate(e.target.value)}
-      className={styles.dropdownBase}
+      className={`${styles.dropdownBase} ${variantStyle} ${sizeStyle}`}
       defaultValue={defaultValue}
     >
       {options.map(({ display, value }) => (
