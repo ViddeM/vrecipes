61449955dea4c4df92fa4b3bebc059e018dec964
Vidar Magnusson <git@vidarmagnusson.com>
2022-06-18 14:38:46 +0200

    Add new font for unicode character support

--- nextjs-frontend/src/pages/_app.tsx
+++ nextjs-frontend/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../resources/styles/globals.scss";
 import "../resources/styles/source_sans_pro.scss";
+import "../resources/styles/deja_vu_sans.scss";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import {
