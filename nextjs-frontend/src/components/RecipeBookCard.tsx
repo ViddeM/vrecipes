a8cfdfbabebdd8e590ee74d14059df0ad43d1049
Eric Carlsson <cirecirecire3@gmail.com>
2022-06-18 14:47:50 +0200

    Add default recipe book image

--- nextjs-frontend/src/components/RecipeBookCard.tsx
+++ nextjs-frontend/src/components/RecipeBookCard.tsx
@@ -1,6 +1,7 @@
 import { useTranslations } from "../hooks/useTranslations";
 import { RECIPE_BOOKS_BASE_ENDPOINT } from "../api/Endpoints";
 import { ShortRecipeBook } from "../api/ShortRecipeBook";
+import defaultRecipeBookImage from "../../public/default-recipe-book-image.webp";
 import styles from "./RecipeBookCard.module.scss";
 import Link from "next/link";
 import { RecipeImage } from "./RecipeImage";
@@ -16,7 +17,11 @@ const RecipeBookCard = ({ recipeBook }: RecipeBookCardProps) => {
     <Link href={`${RECIPE_BOOKS_BASE_ENDPOINT}/${recipeBook.uniqueName}`}>
       <a className={`noStyleLink card ${styles.recipeBookCard}`}>
         <div className={styles.recipeBookCardImageContainer}>
-          <RecipeImage url={recipeBook.imageLink} border="top" />
+          <RecipeImage
+            defaultImage={defaultRecipeBookImage}
+            url={recipeBook.imageLink}
+            border="top"
+          />
         </div>
         <div className={styles.recipeBookCardContent}>
           <h3 className={styles.recipeBookCardTitle}>{recipeBook.name}</h3>
